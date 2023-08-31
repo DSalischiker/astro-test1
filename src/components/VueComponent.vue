3507260d8ffc7265ba2fdf2364ecb7957b257984
Diego Salischiker <diegosalischiker711@gmail.com>
2023-08-31 15:05:47 -0300

    Add files via upload

---
+++ src/components/VueComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Vue Test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h1 {
+  background-image: var(--accent-gradient-green);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: 400%;
+  background-position: 0%;
+}
+</style>
