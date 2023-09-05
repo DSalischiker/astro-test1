8782d40bacd031db2476efd53d519031412e9a66
DSalischiker <diego@studiohakuna.com>
2023-09-05 15:51:00 -0300

    change react component

--- src/components/ReactComponent.jsx
+++ src/components/ReactComponent.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 export default function ReactComponent() {
-    return (<h2 className="react-gradient">This is a React component.</h2>);
+    return (
+        <div>
+            <h2 className="react-gradient">This is a React component!</h2>
+        </div>
+    );
 }
 
